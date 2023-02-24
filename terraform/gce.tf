fdda73a6170e41bb5dc5834ef715162e0c89d644
Manoj Kumar S D <manoj.kumarsd@rackspace.com>
2023-02-24 15:52:36 +0530

    fmt error

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -1,8 +1,8 @@
 # GCE Disk Usage Alerting
 
 resource "google_monitoring_alert_policy" "alert_policy" {
-  display_name = "GCE Disk Usage"
-  combiner     = "OR"
+  display_name          = "GCE Disk Usage"
+  combiner              = "OR"
   notification_channels = ["projects/de-manoj-kumar/notificationChannels/13570824548837170415"]
   documentation {
     content = "This is test runbook for GCP Alerting"
@@ -10,15 +10,15 @@ resource "google_monitoring_alert_policy" "alert_policy" {
   conditions {
     display_name = "GCE Disk Usage"
     condition_threshold {
-      filter     = "metric.type=\"agent.googleapis.com/disk/percent_used\" AND resource.type=\"gce_instance\""
-      duration   = "60s"
-      comparison = "COMPARISON_GT"
+      filter          = "metric.type=\"agent.googleapis.com/disk/percent_used\" AND resource.type=\"gce_instance\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
       threshold_value = 90
       aggregations {
-        alignment_period   = "300s"
-        per_series_aligner = "ALIGN_MEAN"
+        alignment_period     = "300s"
+        per_series_aligner   = "ALIGN_MEAN"
         cross_series_reducer = "REDUCE_MEAN"
-        group_by_fields = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
+        group_by_fields      = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
       }
     }
   }
@@ -27,8 +27,8 @@ resource "google_monitoring_alert_policy" "alert_policy" {
 
 
 resource "google_monitoring_alert_policy" "alert_policy_gce_mem" {
-  display_name = "GCE Mem Usage"
-  combiner = "OR"
+  display_name          = "GCE Mem Usage"
+  combiner              = "OR"
   notification_channels = ["projects/de-manoj-kumar/notificationChannels/13570824548837170415"]
   documentation {
     content = " This is test Runbook for GCE Mem alerting"
@@ -36,25 +36,25 @@ resource "google_monitoring_alert_policy" "alert_policy_gce_mem" {
   conditions {
     display_name = "GCE Mem Usage"
     condition_threshold {
-      filter = "metric.type=\"agent.googleapis.com/memory/percent_used\" AND resource.type=\"gce_instance\""
-      duration = "60s"
-      comparison = "COMPARISON_GT"
+      filter          = "metric.type=\"agent.googleapis.com/memory/percent_used\" AND resource.type=\"gce_instance\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
       threshold_value = "98"
       aggregations {
-        alignment_period   = "300s"
-        per_series_aligner = "ALIGN_MEAN"
+        alignment_period     = "300s"
+        per_series_aligner   = "ALIGN_MEAN"
         cross_series_reducer = "REDUCE_MEAN"
-        group_by_fields = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
+        group_by_fields      = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
       }
-    } 
+    }
   }
 }
 
 # GCE CPU Usage Alerting
 
 resource "google_monitoring_alert_policy" "alert_policy_gce_cpu" {
-  display_name = "GCE CPU Usage"
-  combiner = "OR"
+  display_name          = "GCE CPU Usage"
+  combiner              = "OR"
   notification_channels = ["projects/de-manoj-kumar/notificationChannels/13570824548837170415"]
   documentation {
     content = "This is test Runbook for GCE CPU alerting"
@@ -62,15 +62,15 @@ resource "google_monitoring_alert_policy" "alert_policy_gce_cpu" {
   conditions {
     display_name = "GCE CPU Usage"
     condition_threshold {
-      filter = "metric.type=\"agent.googleapis.com/cpu/utilization\" AND resource.type=\"gce_instance\""
-      duration = "60s"
-      comparison = "COMPARISON_GT"
+      filter          = "metric.type=\"agent.googleapis.com/cpu/utilization\" AND resource.type=\"gce_instance\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
       threshold_value = "95"
       aggregations {
-        alignment_period   = "300s"
-        per_series_aligner = "ALIGN_MEAN"
+        alignment_period     = "300s"
+        per_series_aligner   = "ALIGN_MEAN"
         cross_series_reducer = "REDUCE_MEAN"
-        group_by_fields = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
+        group_by_fields      = ["resource.label.project_id", "resource.label.instance_id", "resource.label.zone"]
       }
     }
   }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-  
+
 provider "google-beta" {
   project = var.project_id
   region  = var.region

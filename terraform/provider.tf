c537f8facbb46f9de8d9197db6df9d35741f7a6a
Manoj Kumar S D <manoj.kumarsd@rackspace.com>
2023-02-24 12:35:02 +0530

    tf and google version updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,3 +7,15 @@ provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
+
+terraform {
+  required_version = "1.3.7"
+  required_providers {
+    google = {
+      version = "4.48.0"
+    }
+    google-beta = {
+      version = "4.48.0"
+    }
+  }
+}

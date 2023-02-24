b7b4ba9da57d28f908d5b8202ee08ce6a73ff3c3
Manoj Kumar S D <manoj.kumarsd@rackspace.com>
2023-02-24 12:32:59 +0530

    updated tf version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,9 @@
 provider "google" {
-  credentials = file("de-manoj-kumar-c2ff3bc080fe.json")
   project = var.project_id
   region  = var.region
 }
   
 provider "google-beta" {
-  credentials = file("de-manoj-kumar-c2ff3bc080fe.json")
   project = var.project_id
   region  = var.region
 }

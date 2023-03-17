bcc76a994dbf129f79709332542ee3127bfb5e9e
Manoj Kumar S D <manoj.kumarsd@rackspace.com>
2023-03-17 10:11:10 +0530

    armor policy

---
+++ terraform/armor.tf
@@ -0,0 +1,27 @@
+resource "google_compute_security_policy" "policy" {
+  name = "modops-armor"
+
+  rule {
+    action   = "deny(403)"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["35.188.188.161"]
+      }
+    }
+    description = "Deny access"
+  }
+
+  rule {
+    action   = "allow"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+}
